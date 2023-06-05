098bbca3d24ed5fd2acea52fa9630cba4e44e8b2
def <drystunn@gmail.com>
2023-06-05 11:56:27 +0300

    updated

--- cmd/main.go
+++ cmd/main.go
@@ -1,58 +1,21 @@
 package main
 
 import (
-	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 	"log"
-	"net/http"
+	"myproj/site/pkg/handler"
 )
 
-var secret = []byte("secret")
-
-const userKey = "user"
-
 func main() {
-	r := engine()
+	r := handler.Engine()
 
 	r.Use(gin.Logger())
 
-	r.LoadHTMLGlob("templates/*")
+	r.LoadHTMLGlob("templates/*.html")
 
-	r.GET("/", index)
+	r.GET("/", handler.Index)
 
 	if err := r.Run(":8080"); err != nil {
 		log.Fatal("Unable to start:", err)
 	}
-}
-
-func index(c *gin.Context) {
-	session := sessions.Default(c)
-	if session.Get(userKey) != nil {
-		c.Redirect(http.StatusFound, "/private/me")
-		return
-	}
-
-	c.HTML(http.StatusOK, "login.html", nil)
-}
-
-func engine() *gin.Engine {
-	r := gin.Default()
-
-	store := cookie.NewStore(secret)
-	r.Use(sessions.Sessions("mysession", store))
-
-	r.Static("/static", "./static")
-
-	r.GET("/login", showLoginPage)
-	r.POST("/login", login)
-	r.GET("/logout", logout)
-
-	private := r.Group("/private")
-	private.Use(AuthRequired)
-	{
-		private.GET("/me", me)
-		private.GET("/status", status)
-	}
-	return r
 }
