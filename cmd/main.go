5ccd56880d211c2c8c472fdfc7d0e10edb03b34c
def <drystunn@gmail.com>
2023-06-06 16:12:14 +0300

    added db

--- cmd/main.go
+++ cmd/main.go
@@ -3,19 +3,27 @@ package main
 import (
 	"github.com/drew3k/site/pkg/handler"
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 	"log"
 )
 
 func main() {
+	db, err := setupDB()
+	if err != nil {
+		log.Fatal("Failed to setup database:", err)
+		return
+	}
+	defer db.Close()
+
 	r := handler.Engine()
-
 	r.Use(gin.Logger())
-
 	r.LoadHTMLGlob("site/templates/*.html")
 
-	r.GET("/", handler.Index)
+	// Передача соединения с базой данных в обработчики запросов
+	r.GET("/", handler.Index(db))
 
 	if err := r.Run(":8080"); err != nil {
 		log.Fatal("Unable to start:", err)
 	}
+}
 }
